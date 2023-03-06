658a1f4fd17baffabfdd683ded724f75613a40c9
Reuven Martin <jaeger33@gmail.com>
2023-03-06 14:22:20 -0500

    basic api calls

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const dotenv = require('dotenv').config();
-const eBirdKey = process.env.EBIRD_API_KEY;
-
-console.log(eBirdKey);
 
 const latitude = 42.98774;
-const longitude = -81.24036;
+const longitude = -81.24036; 
+const radiusKm = 20;
 
-const eBirdApiUrl = 'https://api.ebird.org/v2/data/obs/geo/recent?'
+const fetchEbirdData = async (lat, long, radius) => {
 
-const url = `${eBirdApiUrl}key=${eBirdKey}&lat=${latitude}&long=${longitude}`
+    const eBirdKey = process.env.EBIRD_API_KEY;       
+    const eBirdApiUrl = 'https://api.ebird.org/v2/data/obs/geo/recent?'
 
-let results = fetch(url)
-results.then(results => console.log(results));
+    let result = await fetch(`${eBirdApiUrl}key=${eBirdKey}&lat=${lat}&lng=${long}&dist=${radius}`);
+    return await result.json();
+};
+
+//fetchEbirdData(latitude, longitude, radiusKm).then(data => console.log(data));
+
+const fetchInatData = async (lat, long, radius) => {
+
+    const iNatApiUrl = 'https://api.inaturalist.org/v1/observations?';
+    let result = await fetch(`${iNatApiUrl}lat=${lat}&lng=${long}&radius=${radius}&order=desc&order_by=created_at`);
+    return await result.json();
+};
+
+//fetchInatData(latitude, longitude, radiusKm).then(data => console.log(data));
